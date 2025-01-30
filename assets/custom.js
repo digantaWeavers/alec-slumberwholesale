64264873f4be7464d193d5f13b9fc3d4fdc1d74c
digantaWeavers <dignta.maity@weavers-web.com>
2025-01-30 11:25:04 +0530

    changes

---
+++ assets/custom.js
@@ -0,0 +1,42 @@
+jQuery(document).ready(function ($) {
+    $(".slick.marquee").slick({
+        speed: 8000,
+        autoplay: true,
+        autoplaySpeed: 0,
+        centerMode: false,
+        cssEase: "linear",
+        slidesToShow: 3,
+        draggable: true,
+        focusOnSelect: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        slidesToScroll: 1,
+        // variableWidth: true,
+        infinite: true,
+        initialSlide: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+});
